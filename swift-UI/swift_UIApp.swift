5cccd383d6e9026ca5e3e032b495260411ced463
Liam Wedge <21100218@virtualwindow.co.za>
2022-09-28 10:20:34 +0200

    Initial Commit

---
+++ swift-UI/swift_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_UIApp.swift
+//  swift-UI
+//
+//  Created by Paige Phelps on 2022/09/28.
+//
+
+import SwiftUI
+
+@main
+struct swift_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
